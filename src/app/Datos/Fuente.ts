ec0cfaefc25aca2a9f7ee83a247c8edd977ae9a1
Marcelo Cattani <marcelocattani96@gmail.com>
2020-12-24 00:10:54 -0300

    finalizado

--- src/app/Datos/Fuente.ts
+++ src/app/Datos/Fuente.ts
@@ -4,60 +4,60 @@ export class Persona {
 
     public personas: IPersona[] = [
         {
-            name: "Estela",
-            lastName : "Garcia", 
-            edad : 55,
-            biography : "Es ama de casa", 
-            telefonos : ["2613245989","154864613"]
+            name: "Alan",
+            lastName: "Turing",
+            edad: 41,
+            biography: "Turing nació en Maida Vale, Londres,8 aunque fue concebido en Chatrapur (India británica). Su padre Julius Mathison Turing (1873-1944) era miembro del cuerpo de funcionarios británicos en la India. Julius y su esposa Ethel querían que su hijo Alan naciera en el Reino Unido y regresaron a Paddington, donde finalmente nació.",
+            telefonos: ["2612531426", "111532615"]
         },
         {
-            name: "Estela",
-            lastName : "Garcia", 
-            edad : 55,
-            biography : "Es ama de casa", 
-            telefonos : ["2613245989","154864613"]
+            name: "Marie",
+            lastName: "Curie",
+            edad: 66,
+            biography: "Marie Curie nació en Varsovia (Polonia) el 7 de noviembre de 1867 y murió en Passy (Francia) el 4 de julio de 1934. Nacida como Maria Salomea Sklodowska, Marie Curie es conocida por ser la primera mujer científica en recibir el Premio Nobel y ser la primera catedrática de la Universidad de la Sorbona de París.",
+            telefonos: ["261452316", "456231526"]
         },
         {
-            name: "Estela",
-            lastName : "Garcia", 
-            edad : 55,
-            biography : "Es ama de casa", 
-            telefonos : ["2613245989","154864613"]
+            name: "Frida",
+            lastName: "Kahlo",
+            edad: 47,
+            biography: "Magdalena Carmen Frida Kahlo Calderón (Coyoacán, 6 de julio de 1907-ibidem, 13 de julio de 1954) fue una pintora mexicana. ... Fue autora de 150 obras, principalmente autorretratos, en los que proyectó sus dificultades por sobrevivir. La obra de Frida y la de su marido, el pintor Diego Rivera, se influyeron mutuamente.",
+            telefonos: ["4561892636", "9463259612", "4596312563", "896452316"]
         },
         {
-            name: "Estela",
-            lastName : "Garcia", 
-            edad : 55,
-            biography : "Es ama de casa", 
-            telefonos : ["2613245989","154864613"]
+            name: "Albert",
+            lastName: "Einstein",
+            edad: 76,
+            biography: "Nació el 14 de marzo de 1879, en Ulm (Alemania). Murió el 18 de abril de 1955, en Princeton (Estados Unidos de América). Albert Einstein es quizá el científico mundialmente más conocido por el desarrollo de la Teoría de la Relatividad que revolucionó la ciencia conocida hasta el siglo XX.",
+            telefonos: ["2614962536", "468956314"]
         },
         {
-            name: "Estela",
-            lastName : "Garcia", 
-            edad : 55,
-            biography : "Es ama de casa", 
-            telefonos : ["2613245989","154864613"]
+            name: "Freddie",
+            lastName: "Mercury",
+            edad: 45,
+            biography: "Freddie Mercury (nacido como Farrokh Bulsara;​​ Stone Town, Zanzíbar; 5 de septiembre de 1946-Kensington, Londres; 24 de noviembre de 1991) fue un cantante, compositor, pianista, diseñador gráfico​ y músico británico de origen parsi e indio,​ conocido por haber sido el vocalista principal de la banda de rock Queen.",
+            telefonos: ["496152634", "9654125636", "4962531562"]
         },
         {
-            name: "Estela",
-            lastName : "Garcia", 
-            edad : 55,
-            biography : "Es ama de casa", 
-            telefonos : ["2613245989","154864613"]
+            name: "Johnny",
+            lastName: "Depp",
+            edad: 57,
+            biography: "John Christopher Depp II es un actor, productor, guionista y músico estadounidense. Ha sido nominado en tres ocasiones al Óscar y recibió un Globo de Oro, ​ un premio del Sindicato de Actores y un César.​ Wikipedia",
+            telefonos: ["2613245989", "154864613"]
         },
         {
-            name: "Estela",
-            lastName : "Garcia", 
-            edad : 55,
-            biography : "Es ama de casa", 
-            telefonos : ["2613245989","154864613"]
+            name: "Mercedes",
+            lastName: "Sosa",
+            edad: 74,
+            biography: "Haydée Mercedes Sosa ​ fue una cantante de música folclórica argentina, considerada la mayor exponente del folklore argentino. Se la conoció como La Voz de América Latina.​Fundadora del Movimiento del Nuevo Cancionero y una de las exponentes de la Nueva canción latinoamericana.",
+            telefonos: ["1253626325"]
         },
         {
-            name: "Estela",
-            lastName : "Garcia", 
-            edad : 55,
-            biography : "Es ama de casa", 
-            telefonos : ["2613245989","154864613"]
+            name: "Charly",
+            lastName: "García",
+            edad: 69,
+            biography: "Carlos Alberto García, conocido por su nombre artístico Charly García es un cantautor, compositor, músico y productor argentino de rock, considerado uno de los músicos más importantes de Latinoamérica. Su obra le valió el reconocimiento en muchas partes del mundo",
+            telefonos: ["2613245989", "154864613","26135632638"]
         },
     ]
 
